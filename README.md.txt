2b19172f0be737a9c9cf3af6944aa1f1a2485451
Arnaud Boulianne <arnaud@profasee.com>
2022-10-17 17:14:02 -0400

    This is the one

---
+++ README.md.txt
@@ -0,0 +1,5 @@
+# spring-boot-starter-boilerplate
+
+Boilerplate code for a spring-boot-starter library
+
+All dependencies up-to-date (2022-10-17)
---
+++ src/main/java/com/rndbblnn/stonks/commons/autoconfigure/CommonsAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.rndbblnn.stonks.commons.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonsAutoConfiguration {
+
+}
